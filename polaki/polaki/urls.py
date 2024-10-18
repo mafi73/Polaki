1ab95ea53c2915057b6179f5fb5c164a770cacd7
Mohammadreza <m.mafi73@gmail.com>
2024-10-18 16:16:29 +0330

    اتصال صفخات به یکدیگر انجام شدو دکمه لاگ اوت در تمام صفحات قرارا داده شد.

--- polaki/polaki/urls.py
+++ polaki/polaki/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', auth_views.LogoutView.as_view(), name='logout'),
     path('wallet/', include('wallet.urls')),
     path('transactions/', include('transactions.urls')),
     path('signup/', include('users.urls'))
--- polaki/transactions/form.py
+++ polaki/transactions/form.py
@@ -3,4 +3,4 @@ from .models import Transaction
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
-        fields = ['amount', 'category', 'transaction_type']
+        fields = ['amount', 'category', 'transaction_type', 'description']
