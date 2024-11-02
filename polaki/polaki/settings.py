3085f8eb1b81fd2afcb6f54a9ab1f5cdeee430e4
Mafi <m.mafi73@gmail.com>
2024-11-02 23:54:11 +0330

    migrate to postqres database

--- polaki/polaki/settings.py
+++ polaki/polaki/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'polaki.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'polaki',
+        'USER': 'mafi2',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
